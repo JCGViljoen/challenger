f902e354afcafb43acbdb544f1cd0d191c084597
JCGViljoen <jcgviljone51@gmail.com>
2023-08-11 16:13:33 +0200

    db connection

--- api/config/index.js
+++ api/config/index.js
@@ -1,1 +1,14 @@
-// database configuration
+require("dotenv").config()
+const {createPool} = require("mysql")
+const connection = createPool({
+    host: process.env.dbHost,
+    database: process.env.dbName,
+    user: process.env.dbUser,
+    password: process.env.dbPswd,
+    multiplestatements: true,
+    connectionLimit: 30
+})
+
+module.exports = {
+    connection
+}
